9ab2897e0761c2ae44f3ae450e7f9a7015d3bd4f
McKade <10718916@my.uvu.edu>
2021-02-01 21:55:55 -0700

    Adding Basic throw cupcake script

---
+++ Assets/Scripts/Player/FireCupcake.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireCupcake : MonoBehaviour
+{
+    public float speed = 5f;
+    public GameObject Cupcake;
+    public Transform aimObject;
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject spawnedCupcake = Instantiate(Cupcake, aimObject.position, aimObject.rotation);
+            spawnedCupcake.GetComponent<Rigidbody>().velocity = speed * aimObject.forward;
+
+        }
+    }
+
+
+}
